ca27bfd21e597ceed7127632015299e3e66b218a
kchu100 <68346783+kchu100@users.noreply.github.com>
2022-03-10 11:44:53 -0500

    Create bubble sort.cpp

---
+++ bubble sort.cpp
@@ -0,0 +1,17 @@
+template<class T>
+void BubbleSort(T a[], int count)
+{
+	int temp; //dummy variable
+	
+	for(int x = 0; x < count; x++)
+		for(int y = 0; y < count - 1; y++)
+			if(a[y] >  a[y + 1]) //sorts in ascending order
+			{
+				temp = a[y];
+				a[y] = a[y + 1];
+				a[y + 1] = temp;
+			}
+	
+	cout << "\nSorted Array using bubble sort" << endl;
+	display(a, count);
+}
