529d2a13901fac0dcbc334d3735d56debe8e0370
kchu100 <68346783+kchu100@users.noreply.github.com>
2022-03-14 15:23:06 -0400

    Update bubble sort.cpp

--- Sorting/bubble sort.cpp
+++ Sorting/bubble sort.cpp
@@ -1,7 +1,6 @@
 void BubbleSort(int a[], int count)
 {
 	int temp; //dummy variable
-	
 	for(int x = 0; x < count; x++)
 		for(int y = 0; y < count - 1; y++)
 			if(a[y] >  a[y + 1]) //sorts in ascending order
