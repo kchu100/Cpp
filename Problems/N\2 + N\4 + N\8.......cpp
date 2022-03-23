d16ca300f28c42369c990bff2fd3f53b600e3708
kchu100 <68346783+kchu100@users.noreply.github.com>
2022-03-23 16:18:20 -0400

    Update N\2 + N\4 + N\8.......cpp

--- Problems/N\2 + N\4 + N\8.......cpp
+++ Problems/N\2 + N\4 + N\8.......cpp
@@ -1,9 +1,25 @@
+/*
+Compute N/1 + N/2 + N/4 + N/8 + N/16.... series 
+Example:
+N = 35
+35 + 35/2 + 35/4 + 35/8 + 35/16 + 35/32 = 35 + 17 + 8 + 4 + 2 + 1 = 67
+*/
 int computeSeries(int n)
 {
-    int result, divisor = 1;
+    int result = 0, divisor = 1;
+    /*traditional loop
+    while(divisor < n)
+    {
+        result += n/divisor;
+        divisor *= 2;
+    }
+    return result;
+    */
+    
+    /*using Recursion
     divisor *= 2;
     if(divisor > n)
         return n;
-    else
-        return n + computeSeries(n/divisor);
+    return n + computeSeries(n/divisor);
+    */
 }
