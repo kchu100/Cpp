158ffe69ac87d8c673c00a285ab675041e561126
kchu100 <68346783+kchu100@users.noreply.github.com>
2022-03-23 11:48:56 -0400

    Create Tree Class.cpp

---
+++ Data Structures/Trees/Tree Class.cpp
@@ -0,0 +1,11 @@
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
