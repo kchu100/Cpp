5ad399d641e4c3ab67171e628181b70d3f4488df
kchu100 <68346783+kchu100@users.noreply.github.com>
2022-03-23 11:50:20 -0400

    Create Insert.cpp

---
+++ Data Structures/Trees/Insert.cpp
@@ -0,0 +1,12 @@
+//Insertion of a new node
+    Node * insert(Node * root, int data) {
+        if(root == NULL)
+            return new Node(data);
+
+        if(root->data > data)
+            root->left = insert(root->left, data);
+        else
+            root->right = insert(root->right, data);
+
+        return root;
+    }
