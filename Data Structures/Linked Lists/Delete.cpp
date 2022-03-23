66732e4dad783b1cc3e76fe208480674f93901f8
kchu100 <68346783+kchu100@users.noreply.github.com>
2022-03-23 11:40:41 -0400

    Update Delete.cpp

--- Data Structures/Linked Lists/Delete.cpp
+++ Data Structures/Linked Lists/Delete.cpp
@@ -23,3 +23,42 @@ SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* head, int position)
     }
     return head;
 }
+
+//Delete duplicate nodes from a sorted list
+SinglyLinkedListNode* removeDuplicates(SinglyLinkedListNode* head)
+{
+    SinglyLinkedListNode *current = head;
+    SinglyLinkedListNode *prev = current;
+    SinglyLinkedListNode *n = current->next;
+    SinglyLinkedListNode *del;
+    
+    while(n != NULL)
+    {
+        if(head->data == head->next->data)
+        {
+            del = current;
+            head = head->next;
+            current = head;
+            n = current->next;
+            prev = current;
+            del->next = nullptr;
+            delete del;
+        }
+        else if(current->data == n->data)
+        {
+            del = current;
+            current = n;
+            n = current->next;
+            prev->next = current;
+            del->next = nullptr;
+            delete del;
+        }
+        else
+        {
+            prev = current;
+            current = current->next;
+            n = current->next;
+        }
+    }
+    return head;
+}
