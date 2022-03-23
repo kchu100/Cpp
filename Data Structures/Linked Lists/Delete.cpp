b674a509331d7740d20d24f70a95e1021808f5db
kchu100 <68346783+kchu100@users.noreply.github.com>
2022-03-23 11:37:07 -0400

    Create Delete.cpp

---
+++ Data Structures/Linked Lists/Delete.cpp
@@ -0,0 +1,24 @@
+SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* head, int position) 
+{
+    SinglyLinkedListNode *curr, *del;
+    if(position == 0)
+    {
+        del = head;
+        head = head->next;
+        delete del;
+    }
+    else 
+    {
+        del = head;
+        for(int i = 0; i < position; i++)
+        {
+            curr = del;
+            del = del->next;
+        }
+
+        curr->next = del->next;
+        delete del;
+    
+    }
+    return head;
+}
