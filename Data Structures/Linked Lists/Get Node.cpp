d9321b0e86683bc772f539c64339024949271f45
kchu100 <68346783+kchu100@users.noreply.github.com>
2022-03-23 11:42:32 -0400

    Update Get Node.cpp

--- Data Structures/Linked Lists/Get Node.cpp
+++ Data Structures/Linked Lists/Get Node.cpp
@@ -15,3 +15,23 @@ int getNode(SinglyLinkedListNode* head, int positionFromTail) {
     value = curr->data;
     return value;
 }
+
+//Find the merge point of two joined lists
+int findMergeNode(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+    SinglyLinkedListNode *current1 = head1;
+    SinglyLinkedListNode *current2 = head2;
+    
+    while(current1 != current2)
+    {
+        if(current1->next == NULL)
+            current1 = head1;
+        else    
+            current1 = current1->next;
+        
+        if(current2->next == NULL)
+            current2 = head2;
+        else
+            current2 = current2->next;
+    }
+    return current1->data;
+}
