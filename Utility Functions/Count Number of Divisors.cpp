4e4b17b261dfe5e34d20d7ab8bbfafcb7c51491a
kchu100 <68346783+kchu100@users.noreply.github.com>
2022-03-28 12:47:33 -0400

    Create Count Number of Divisors.cpp

---
+++ Utility Functions/Count Number of Divisors.cpp
@@ -0,0 +1,16 @@
+int countDivisors(int n)
+{
+  int count = 0;
+  for(int i = 1; i <= sqrt(n); i++)
+  {
+    if(n % i == 0)
+    {
+      //f divisors are equal count only one, otherswise count two.
+      if(n / i == i)
+        count++;
+      else
+        count += 2;
+    }
+  }
+  return count;
+}
