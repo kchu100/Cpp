64e8a20a9ffb469afb4775067288da5b7b42f268
kchu100 <68346783+kchu100@users.noreply.github.com>
2022-03-28 13:01:52 -0400

    Update Count Number of Divisors.cpp

--- Utility Functions/Count Number of Divisors.cpp
+++ Utility Functions/Count Number of Divisors.cpp
@@ -15,3 +15,15 @@ int countDivisors(int n)
   }
   return count;
 }
+
+
+/*
+int countDivisors(int n)
+{
+  int count = 0;
+  for(int i = 1; i <= n; i++)
+    if(n % i == 0)
+        count++;
+  return count;
+}
+*/
