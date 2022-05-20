859b69d5a88c41cb0ff3186056f7b733ca7afdb1
kchu100 <68346783+kchu100@users.noreply.github.com>
2022-05-20 10:14:32 -0400

    Create MiniMax.cpp

---
+++ Problems/MiniMax.cpp
@@ -0,0 +1,18 @@
+/*
+Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. 
+Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+*/
+void miniMaxSum(vector<int> arr) 
+{
+    int min = arr[0], max = arr[0], sum = 0;
+    for(int i = 0; i < arr.size(); i++)
+    {
+        sum += arr[i];
+        if(arr[i] > max)
+            max = arr[i];
+        if(arr[i] < min)
+            min = arr[i];
+    }
+    //cout << Minimum << Maximum
+    cout << sum - max << ' ' << sum - min;
+}
