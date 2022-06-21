0d821451a3ea3e9a90971d472de1808b243d0d5e
kchu100 <68346783+kchu100@users.noreply.github.com>
2022-06-21 13:24:29 -0400

    Create Recursive Digit Sum.cpp

---
+++ Problems/Recursive Digit Sum.cpp
@@ -0,0 +1,20 @@
+/*
+We define super digit of an integer 'n' using the following rules:
+Given an integer, we need to find the super digit of the integer.
+If  has only  digit, then its super digit is.
+Otherwise, the super digit of  is equal to the super digit of the sum of the digits of.+
+Calculate the sum of digits of n * k initially
+*/
+  
+int superDigit(int n, int k) 
+{
+    int x = n, sum = 0;
+    sum = sumDigits(x);
+    sum *= k;
+
+    if(sum < 10)
+    {
+        return sum;
+    }
+    return superDigit(n, 1);
+}
