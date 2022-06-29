fff490b9acb088c1f4eb07f347650fe546a8bc52
kchu100 <68346783+kchu100@users.noreply.github.com>
2022-06-29 12:44:58 -0400

    Update Recursive Digit Sum.cpp

--- Problems/Recursive Digit Sum.cpp
+++ Problems/Recursive Digit Sum.cpp
@@ -4,7 +4,7 @@ Given an integer, we need to find the super digit of the integer.
 If  has only  digit, then its super digit is.
 Otherwise, the super digit of  is equal to the super digit of the sum of the digits of.+
 Calculate the sum of digits of n * k initially
-Input is a string
+Input is a string of integers
 */
   
 int sumDigits(string x)
